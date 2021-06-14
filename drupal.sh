199cb10a31a462b9ffe5c9ddcd85e152723370a0
Matthias Bauw <matthias.bauw@gmail.com>
2021-06-14 19:55:33 +0200

    changed profile to root instead of vagrant

--- drupal.sh
+++ drupal.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /var/www
 php -d memory_limit=-1 /usr/local/bin/composer install
-echo "alias drush='/wwwroot/vendor/drush/drush/drush'" >> /home/vagrant/.bashrc
-echo "alias drupal='/wwwroot/vendor/drupal/console/bin/drupal'" >> /home/vagrant/.bashrc
+echo "alias drush='/wwwroot/vendor/drush/drush/drush'" >> /home/root/.bashrc
+echo "alias drupal='/wwwroot/vendor/drupal/console/bin/drupal'" >> /home/root/.bashrc
 
