e34224bbc990499cdf13c3e4f95358cd75139a75
Matthias Bauw <matthias.bauw@gmail.com>
2021-06-14 20:21:05 +0200

    drupal installation added to drupal.sh

--- drupal.sh
+++ drupal.sh
@@ -3,4 +3,5 @@ cd /var/www
 php -d memory_limit=-1 /usr/local/bin/composer install
 echo "alias drush='/wwwroot/vendor/drush/drush/drush'" >> /home/root/.bashrc
 echo "alias drupal='/wwwroot/vendor/drupal/console/bin/drupal'" >> /home/root/.bashrc
-
+./vendor/drush/drush/drush -y si minimal --db-url=mysql://root:vagrant@localhost/drupal --config-dir=/wwwroot/config --account-pass=admin
+./vendor/drush/drush/drush -y cr
