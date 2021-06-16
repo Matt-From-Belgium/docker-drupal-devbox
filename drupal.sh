5cb354211d3e3e278a18863d874985d7fea29edc
Matthias Bauw <matthias.bauw@gmail.com>
2021-06-16 20:12:30 +0200

    apache 2 foreground zit nu in drupal script

--- drupal.sh
+++ drupal.sh
@@ -5,3 +5,4 @@ echo "alias drush='/wwwroot/vendor/drush/drush/drush'" >> /home/root/.bashrc
 echo "alias drupal='/wwwroot/vendor/drupal/console/bin/drupal'" >> /home/root/.bashrc
 ./vendor/drush/drush/drush -y si minimal --db-url=mysql://root:vagrant@localhost/drupal --config-dir=/wwwroot/config --account-pass=admin
 ./vendor/drush/drush/drush -y cr
+apache2-foreground
