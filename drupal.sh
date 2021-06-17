814fb74dabec7c3d73380ab547d821c2a794216b
Matthias Bauw <matthias.bauw@gmail.com>
2021-06-17 13:40:01 +0200

    changed root path in drush install

--- drupal.sh
+++ drupal.sh
@@ -3,6 +3,6 @@ cd /var/www
 php -d memory_limit=-1 /usr/local/bin/composer install
 echo "alias drush='/wwwroot/vendor/drush/drush/drush'" >> /home/root/.bashrc
 echo "alias drupal='/wwwroot/vendor/drupal/console/bin/drupal'" >> /home/root/.bashrc
-./vendor/drush/drush/drush -y si minimal --db-url=mysql://root:secret@mysql-server/drupal --config-dir=/var/www/config --account-pass=admin
+./vendor/drush/drush/drush -y si minimal --root=/var/www/web --db-url=mysql://root:secret@mysql-server/drupal --config-dir=/var/www/config --account-pass=admin
 ./vendor/drush/drush/drush -y cr
 apache2-foreground
