8bc15c7e1c14da54d22737b49a43eff997a6d9a4
Matthias Bauw <matthias.bauw@gmail.com>
2021-06-17 13:49:54 +0200

    error in symlink targets

--- drupal.sh
+++ drupal.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cd /var/www
 php -d memory_limit=-1 /usr/local/bin/composer install
-echo "alias drush='/wwwroot/vendor/drush/drush/drush'" >> /home/root/.bashrc
-echo "alias drupal='/wwwroot/vendor/drupal/console/bin/drupal'" >> /home/root/.bashrc
+echo "alias drush='/var/www/vendor/drush/drush/drush'" >> /home/root/.bashrc
+echo "alias drupal='/var/www/vendor/drupal/console/bin/drupal'" >> /home/root/.bashrc
 ./vendor/drush/drush/drush -y si minimal --root=/var/www/web --db-url=mysql://root:secret@mysql-server/drupal --config-dir=/var/www/config --account-pass=admin
 ./vendor/drush/drush/drush -y cr
 apache2-foreground
