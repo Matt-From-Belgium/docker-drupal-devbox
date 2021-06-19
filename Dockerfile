8b49c2013ccc90c374dd49672636c1a76a9abf61
Matthias Bauw <matthias.bauw@gmail.com>
2021-06-19 11:38:32 +0200

    moved drupal scripts to matthiasba/docker-drupal-testenv"

--- Dockerfile
+++ Dockerfile
@@ -25,8 +25,6 @@ RUN php composer-setup.php
 RUN php -r "unlink('composer-setup.php');"
 RUN mv composer.phar /usr/local/bin/composer
 RUN export COMPOSER_MEMORY_LIMIT=-1
-COPY ./drupal-entrypoint.sh /home/root
-COPY ./drupal-install.sh /home/root
 RUN rm -rf /var/www/html
 RUN ln -s /var/www/web /var/www/html
 WORKDIR /
--- drupal-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-CONTAINER_ALREADY_STARTED="CONTAINER_ALREADY_STARTED_PLACEHOLDER"
-if [ ! -e $CONTAINER_ALREADY_STARTED ]; then
-    touch $CONTAINER_ALREADY_STARTED
-    echo "-- First container startup --"
-      sh /home/root/drupal-install.sh
-else
-    echo "-- Not first container startup --"
-fi
-apache2-foreground
